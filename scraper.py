f40cbf8f3e93770878f2b07770a54267fb42a0d4
Anikate-De <anikate1de@gmail.com>
2022-09-09 17:09:05 +0530

    implement barebone choice input

--- scraper.py
+++ scraper.py
@@ -5,6 +5,56 @@ import praw
 from dotenv import load_dotenv
 from termcolor import colored
 
+
+def loopThroughSubmissions():
+    for submission in submissions:
+
+        if submission.over_18:
+            print('\n', colored(text='NSFW', color='red',
+                                attrs=['bold']), sep='', end='\n\n')
+
+        print(colored(text='Title:', color='yellow'), colored(
+            text=submission.title, attrs=['bold']), end='\n\n')
+        print(colored(text='Desc:', color='yellow'),
+              submission.selftext, end='\n\n')
+
+        print('Upvotes:', colored(text=submission.score, color='green'))
+        print('ID', submission.id, end='\n\n\n')
+
+        while True:
+            
+            choice = input(colored(text='waiting for input... ',
+                                   color='cyan', attrs=['bold']))
+
+            # Remove the input line with the below ANSI codes
+            print('\033[1A' + '\033[K', end='')
+            print('\033[1A' + '\033[K', end='')
+
+            match choice.lower():
+                case 'q':
+                    cho = input(
+                        f'The data will be saved, confirm quit? {colored(text="[y/n]", color="yellow", attrs=["bold"])} ')
+                    if cho.lower() == 'y':
+                        print('Saving, please wait...')
+                        # Execute saving logic here
+                        print('\033[1A' + '\033[K', end='')
+                        print('Saved')
+                        return
+                case 'a':
+                    # Execute addition logic here
+                    print('Added')
+                    print()
+                    break
+                case 'e':
+                    # Add editing logic here
+                    print('Edited')
+                    print()
+                    break
+                case _:
+                    print('Invalid input, please enter one of the following (q, a, e)')
+            
+
+
 # Use ctypes to enable ANSI codes in a terminal
 kernel32 = ctypes.windll.kernel32
 kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
@@ -25,29 +75,7 @@ print(reddit.auth.url(scopes=["identity"], state="...", duration="permanent"))
 
 reddit.read_only = True
 
-while True:
+# Lazy-load all submissions in the subreddit
+submissions = reddit.subreddit('DadJokes').top(limit=float('inf'))
 
-    # Lazy-load all submissions in the subreddit
-    submissions = reddit.subreddit('DadJokes').top(limit=float('inf'))
-
-    for submission in submissions:
-
-        if submission.over_18:
-            print('\n', colored(text='NSFW', color='red',
-                  attrs=['bold']), sep='', end='\n\n')
-
-        print(colored(text='Title:', color='yellow'), colored(
-            text=submission.title, attrs=['bold']), end='\n\n')
-        print(colored(text='Desc:', color='yellow'),
-              submission.selftext, end='\n\n')
-
-        print('Upvotes:', colored(text=submission.score, color='green'))
-        print('ID', submission.id, end='\n\n')
-
-        input(colored(text='waiting for input... ',
-              color='cyan', attrs=['bold']))
-
-        # TODO: Implement input based conditional actions
-
-        # Remove the input line with the below ANSI codes
-        print('\033[1A' + '\033[K', end='')
+loopThroughSubmissions()
