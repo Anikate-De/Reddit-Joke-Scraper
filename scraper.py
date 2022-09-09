015f14d3412a9d77e49e1ed803a8897a4cc9e51e
Anikate-De <anikate1de@gmail.com>
2022-09-09 12:23:16 +0530

    format output with ANSI

--- scraper.py
+++ scraper.py
@@ -1,7 +1,13 @@
+import ctypes
 import os
 
 import praw
 from dotenv import load_dotenv
+from termcolor import colored
+
+# Use ctypes to enable ANSI codes in a terminal
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 load_dotenv()
 
@@ -21,16 +27,27 @@ reddit.read_only = True
 
 while True:
 
-    submissions = reddit.subreddit('DadJokes').top(limit = float('inf'))
+    # Lazy-load all submissions in the subreddit
+    submissions = reddit.subreddit('DadJokes').top(limit=float('inf'))
 
     for submission in submissions:
-        print(submission.title)
-        print(submission.selftext)
-        print()
 
-        print('NSFW', submission.over_18)
-        print('Upvotes', submission.score)
-        print('ID', submission.id)
-        print()
+        if submission.over_18:
+            print('\n', colored(text='NSFW', color='red',
+                  attrs=['bold']), sep='', end='\n\n')
 
-        input('Enter to get to next joke _')
+        print(colored(text='Title:', color='yellow'), colored(
+            text=submission.title, attrs=['bold']), end='\n\n')
+        print(colored(text='Desc:', color='yellow'),
+              submission.selftext, end='\n\n')
+
+        print('Upvotes:', colored(text=submission.score, color='green'))
+        print('ID', submission.id, end='\n\n')
+
+        input(colored(text='waiting for input... ',
+              color='cyan', attrs=['bold']))
+
+        # TODO: Implement input based conditional actions
+
+        # Remove the input line with the below ANSI codes
+        print('\033[1A' + '\033[K', end='')
